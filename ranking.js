44f25455b280c6a3bf64c8c0ddf2629376cdda18
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-07-24 14:48:45 -0300

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -98,12 +98,12 @@ function addDataToTable(user, userData, initialPower, rank, positionChange) {
         </td>
         <td data-label="Link">
             <a href="https://rollercoin.com/p/${user.link}" class="btn-home" target="_blank">
-                <img src="images/botao-home.png" alt="Botão Home" class="btn-home-img" style="width: 35px; height: 35px;>
+                <img src="images/botao-home.png" alt="Botão Home" class="btn-home-img" style="width: 35px; height: 35px;">
             </a>
         </td>
         <td data-label="Farm">
             <a href="https://wminerrc.github.io/calculator/index.html?user=${user.link}" class="btn-home" target="_blank">
-                <img src="images/calculadora.png" alt="Calculadora" class="btn-home-img" style="width: 35px; height: 35px; "border-radius: 0;">
+                <img src="images/calculadora.png" alt="Calculadora" class="btn-home-img" style="width: 35px; height: 35px; border-radius: 0;">
             </a>
         </td>    
     `;
