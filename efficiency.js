70b0de5acef256dd2cc2ff113ba2684b7a11ca12
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-07-25 01:58:24 -0300

    Update efficiency.js

--- efficiency.js
+++ efficiency.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Atualize os resultados na tabela
         document.getElementById('newMiners').textContent = convertPower(newMiners);
-        document.getElementById('newBonuspercent').textContent = `${(newBonuspercent).toFixed(2).replace('.', ',')}%`;
+        document.getElementById('newBonuspercent').textContent = `${(newBonuspercent * 100).toFixed(2).replace('.', ',')}%`;
         document.getElementById('newBonus').textContent = convertPower(newBonus);
 
         // Determine a cor e a seta para o newPower
