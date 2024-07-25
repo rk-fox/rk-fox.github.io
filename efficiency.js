3fb781932dc95dcdbe0d0290a9919e2b78de0967
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-07-25 15:58:23 -0300

    Update efficiency.js

--- efficiency.js
+++ efficiency.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Função para converter poder
     function convertPower(value) {
-        if (value >= 1e9) return (value / 1e9).toFixed(2) + ' EHs';
         if (value >= 1e6) return (value / 1e6).toFixed(2) + ' PHs';
         if (value >= 1e3) return (value / 1e3).toFixed(2) + ' THs';
         return value + ' GHs';
