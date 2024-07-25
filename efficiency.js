dad44658796628b53b2118de0e3294f6301ed01a
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-07-25 00:20:22 -0300

    Update efficiency.js

--- efficiency.js
+++ efficiency.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let buyBonus = parseFloat(document.getElementById('buyBonus').value) || 0;
 
         // Calcule os novos valores
-        let newMiners = miners - sellPower + buyPower;
+        let newMiners = data.miners - (sellPower * 1000) + (buyPower * 1000);
         let newBonuspercent = bonusPercent - sellBonus + buyBonus;
         let newBonus = newMiners * newBonuspercent / 100;
         let newPower = newMiners + newBonus;
