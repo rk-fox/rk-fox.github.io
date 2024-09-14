ff2dd6c9147f6701ee121e9f10d47a643e92a69c
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-09-14 00:33:31 -0300

    Update impact.js

--- impacto/impact.js
+++ impacto/impact.js
@@ -39,13 +39,13 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             // Exibir avatar e mensagem de boas-vindas
-            const avatarUrl = https://avatars.rollercoin.com/static/avatars/thumbnails/50/${avatarId}.png;
+            const avatarUrl = `https://avatars.rollercoin.com/static/avatars/thumbnails/50/${avatarId}.png`;
             document.getElementById('avatar').src = avatarUrl;
             document.getElementById('avatar').style.display = 'block';  // Tornar a imagem visível
             document.getElementById('welcomeMessage').innerText = Olá, ${userName}!;
 
             // Buscar dados de user-power-data usando avatarId
-            const powerDataResponse = await fetch(https://rollercoin.free.mockoapp.net/get?url=https://rollercoin.com/api/profile/user-power-data/${avatarId});
+            const powerDataResponse = await fetch(`https://rollercoin.free.mockoapp.net/get?url=https://rollercoin.com/api/profile/user-power-data/${avatarId}`);
             const powerData = await powerDataResponse.json();
             const powerContents = JSON.parse(powerData.contents);
             const miners = powerContents.data.miners;
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const total_orig = miners * (1 + (bonusPercent / 100));
 
             // Buscar dados de room-config usando avatarId
-            const roomConfigResponse = await fetch(https://rollercoin.free.mockoapp.net/get?url=https://rollercoin.com/api/game/room-config/${avatarId});
+            const roomConfigResponse = await fetch(`https://rollercoin.free.mockoapp.net/get?url=https://rollercoin.com/api/game/room-config/${avatarId}`);
             const roomConfigData = await roomConfigResponse.json();
             const roomConfigContents = JSON.parse(roomConfigData.contents);
 
@@ -118,7 +118,7 @@ console.log(topThreeNegatives);
             // Preencher os dados na tabela HTML
             if (topThreeNegatives.length > 0) {
                 document.getElementById('nome1').innerText = topThreeNegatives[0]?.name || '';
-                document.getElementById('img1').src = https://static.rollercoin.com/static/img/market/miners/${topThreeNegatives[0].filename}.gif?v=1;
+                document.getElementById('img1').src = `https://static.rollercoin.com/static/img/market/miners/${topThreeNegatives[0].filename}.gif?v=1`;
                 document.getElementById('img1').style.display = 'block';  // Tornar a imagem visível
                 document.getElementById('poder1').innerText = topThreeNegatives[0] ? convertPower2(topThreeNegatives[0].power) : '';
                 document.getElementById('bonus1').innerText = topThreeNegatives[0] ? ${(topThreeNegatives[0].bonus_percent / 100).toFixed(2).replace('.', ',')}% : '';
@@ -126,7 +126,7 @@ console.log(topThreeNegatives);
                 document.getElementById('set1').innerText = topThreeNegatives[0] ? (topThreeNegatives[0].is_in_set ? 'Sim' : 'Não') : '';
                 
                 document.getElementById('nome2').innerText = topThreeNegatives[1]?.name || '';
-                document.getElementById('img2').src = https://static.rollercoin.com/static/img/market/miners/${topThreeNegatives[1].filename}.gif?v=1;
+                document.getElementById('img2').src = `https://static.rollercoin.com/static/img/market/miners/${topThreeNegatives[1].filename}.gif?v=1`;
                 document.getElementById('img2').style.display = 'block';  // Tornar a imagem visível
                 document.getElementById('poder2').innerText = topThreeNegatives[1] ? convertPower2(topThreeNegatives[1].power) : '';
                 document.getElementById('bonus2').innerText = topThreeNegatives[1] ? ${(topThreeNegatives[1].bonus_percent / 100).toFixed(2).replace('.', ',')}% : '';
@@ -134,7 +134,7 @@ console.log(topThreeNegatives);
                 document.getElementById('set2').innerText = topThreeNegatives[1] ? (topThreeNegatives[1].is_in_set ? 'Sim' : 'Não') : '';
                 
                 document.getElementById('nome3').innerText = topThreeNegatives[2]?.name || '';
-                document.getElementById('img3').src = https://static.rollercoin.com/static/img/market/miners/${topThreeNegatives[2].filename}.gif?v=1;
+                document.getElementById('img3').src = `https://static.rollercoin.com/static/img/market/miners/${topThreeNegatives[2].filename}.gif?v=1`;
                 document.getElementById('img3').style.display = 'block';  // Tornar a imagem visível
                 document.getElementById('poder3').innerText = topThreeNegatives[2] ? convertPower2(topThreeNegatives[2].power) : '';
                 document.getElementById('bonus3').innerText = topThreeNegatives[2] ? ${(topThreeNegatives[2].bonus_percent / 100).toFixed(2).replace('.', ',')}% : '';
