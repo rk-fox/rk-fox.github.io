a7adccbb641cc24e7a5de0cb660a46d0dc620934
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-09-15 12:59:04 -0300

    Update craft.js

--- craft/craft.js
+++ craft/craft.js
@@ -10,20 +10,6 @@ function calcular() {
             const precoSpeed1 = parseFloat(document.getElementById('preco-speed1').value);
             const cash1 = parseFloat(document.getElementById('cash1').value);
 
-            // Peca Craft 2
-            const quantidadeCraft2 = parseFloat(document.getElementById('quantidade-craft2').value);
-            const precoPecaCraft2 = parseFloat(document.getElementById('preco-peca-craft2').value);
-            const precoCraft2 = parseFloat(document.getElementById('preco-craft2').value);
-            const precoSpeed2 = parseFloat(document.getElementById('preco-speed2').value);
-            const cash2 = parseFloat(document.getElementById('cash2').value);
-
-            // Peca Craft 3
-            const quantidadeCraft3 = parseFloat(document.getElementById('quantidade-craft3').value);
-            const precoPecaCraft3 = parseFloat(document.getElementById('preco-peca-craft3').value);
-            const precoCraft3 = parseFloat(document.getElementById('preco-craft3').value);
-            const precoSpeed3 = parseFloat(document.getElementById('preco-speed3').value);
-            const cash3 = parseFloat(document.getElementById('cash3').value);
-
             const tabela = document.getElementById('resultado-tabela').getElementsByTagName('tbody')[0];
             tabela.innerHTML = '';  // Limpa a tabela existente
 
@@ -37,20 +23,6 @@ function calcular() {
                 const exchangeCraft1 = custoCraft1 * 0.68;
                 const cashCraft1 = valorCraft1 * cash1;
 
-                // Cálculo para Craft 2
-                const divisorCraft2 = ((quantidadeCraft2 * precoPecaCraft2 * xpRltMarket) + ((precoCraft2 + precoSpeed2) * xpRltCraft)) * multi;
-                const valorCraft2 = Math.ceil(xpDesejado / divisorCraft2);
-                const custoCraft2 = ((quantidadeCraft2 * precoPecaCraft2) + precoCraft2 + precoSpeed2) * valorCraft2;
-                const exchangeCraft2 = custoCraft2 * 0.68;
-                const cashCraft2 = valorCraft2 * cash2;
-
-                // Cálculo para Craft 3
-                const divisorCraft3 = ((quantidadeCraft3 * precoPecaCraft3 * xpRltMarket) + ((precoCraft3 + precoSpeed3) * xpRltCraft)) * multi;
-                const valorCraft3 = Math.ceil(xpDesejado / divisorCraft3);
-                const custoCraft3 = ((quantidadeCraft3 * precoPecaCraft3) + precoCraft3 + precoSpeed3) * valorCraft3;
-                const exchangeCraft3 = custoCraft3 * 0.68;
-                const cashCraft3 = valorCraft3 * cash3;
-
                 // Adiciona uma nova linha à tabela
                 const novaLinha = tabela.insertRow();
 
@@ -59,13 +31,5 @@ function calcular() {
                 novaLinha.insertCell(2).textContent = exchangeCraft1.toFixed(2);
                 novaLinha.insertCell(3).textContent = custoCraft1.toFixed(2);
                 novaLinha.insertCell(4).textContent = cashCraft1.toFixed(2);
-                novaLinha.insertCell(5).textContent = valorCraft2;
-                novaLinha.insertCell(6).textContent = exchangeCraft2.toFixed(2);
-                novaLinha.insertCell(7).textContent = custoCraft2.toFixed(2);
-                novaLinha.insertCell(8).textContent = cashCraft2.toFixed(2);
-                novaLinha.insertCell(9).textContent = valorCraft3;
-                novaLinha.insertCell(10).textContent = exchangeCraft3.toFixed(2);
-                novaLinha.insertCell(11).textContent = custoCraft3.toFixed(2);
-                novaLinha.insertCell(12).textContent = cashCraft3.toFixed(2);
             }
 }
