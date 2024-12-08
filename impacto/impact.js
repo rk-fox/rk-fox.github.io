feb841516ab96e36d02df50449942e944fe4ee27
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-12-08 16:01:21 -0300

    Update impact.js

--- impacto/impact.js
+++ impacto/impact.js
@@ -67,11 +67,18 @@ document.addEventListener('DOMContentLoaded', () => {
             const roomConfigResponse = await fetch(`https://summer-night-03c0.rk-foxx-159.workers.dev/?https://rollercoin.com/api/game/room-config/${avatarId}`);
             const roomConfigData = await roomConfigResponse.json();
             const minerData = roomConfigData.data.miners; // Acessando diretamente roomConfigData.data
+            const racks = roomConfigData.data.racks; // Supondo que racks seja parte do roomConfigData.data
 
-            // Obter valor do rádio selecionado
-            const selectedOption = document.querySelector('input[name="option"]:checked').value;
+            // Associar cada minerador à sua rack
+            minerData.forEach(miner => {
+                const rack = racks.find(r => r.rack_id === miner.rack_id); // Encontrar a rack correspondente
+                if (rack) {
+                    miner.rack_info = rack; // Adiciona as informações da rack ao miner
+                }
+            });
 
             // Filtrar minerData com base na opção selecionada
+            const selectedOption = document.querySelector('input[name="option"]:checked').value;
             let filteredMiners = minerData;
             if (selectedOption === 'op1') {
                 filteredMiners = minerData.filter(miner => miner.width === 1);
@@ -85,7 +92,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     acc[id] = (acc[id] || 0) + 1;
                     return acc;
                 }, {});
-
                 return counts;
             }
 
@@ -111,15 +117,6 @@ document.addEventListener('DOMContentLoaded', () => {
             negativeResults.sort((a, b) => Math.abs(a.newpower) - Math.abs(b.newpower));
             const top10NegativeResults = negativeResults.slice(0, 10);
 
-            // Logar 30 resultados negativos no console
-            const top30NegativeResults = negativeResults.slice(0, 30);
-            console.log(top30NegativeResults.map(miner => ({
-                name: miner.name,
-                power: convertPower(miner.power),
-                bonus: `${(miner.bonus_percent / 100).toFixed(2).replace('.', ',')}%`,
-                newpower: convertPower(miner.newpower)
-            })));
-
             // Atualizar os elementos da página com informações dos mineradores negativos
             const updateElement = (index, miner) => {
                 if (miner) {
@@ -133,6 +130,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     document.getElementById(`impact${index}`).innerText = convertPower(miner.newpower);
                     document.getElementById(`set${index}`).innerText = miner.is_in_set ? 'Sim' : 'Não';
                     document.getElementById(`merge${index}`).innerText = counts[miner.miner_id] > 1 ? 'Sim' : 'Não';
+
+                    // Exibir informações da rack
+                    if (miner.rack_info) {
+                        const rack = miner.rack_info;
+                        document.getElementById(`rack${index}`).innerText = `Sala: ${rack.room_level + 1}, Linha: ${rack.placement.x + 1}, Rack: ${rack.placement.y + 1}`;
+                    }
                 } else {
                     document.getElementById(`nome${index}`).innerText = '';
                 }
