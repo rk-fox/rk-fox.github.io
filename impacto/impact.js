f2b526b6ff57668e992344d61354739dbf5ee428
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-12-08 23:33:54 -0300

    Update impact.js

--- impacto/impact.js
+++ impacto/impact.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
         return (value).toFixed(3).replace('.', ',') + ' GHs';
     }
 
-    // Função para converter poder (apenas na exibição de miner em GHs)
     function convertPower2(value) {
         const absValue = Math.abs(value); // Obter o valor absoluto
         if (absValue >= 1e3) return (value / 1e3).toFixed(3).replace('.', ',') + ' THs';
@@ -34,10 +33,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         try {
-            // Buscar avatar_id
             const profileResponse = await fetch(`https://summer-night-03c0.rk-foxx-159.workers.dev/?https://rollercoin.com/api/profile/public-user-profile-data/${userLink}`); 
             const profileData = await profileResponse.json();
-            const userName = profileData.data.name; // Acessando diretamente profileData.data
+            const userName = profileData.data.name; 
             const avatarId = profileData.data.avatar_id;
 
             if (!avatarId || !userName) {
@@ -45,39 +43,32 @@ document.addEventListener('DOMContentLoaded', () => {
                 return;
             }
 
-            // Exibir avatar e mensagem de boas-vindas
             const avatarUrl = `https://avatars.rollercoin.com/static/avatars/thumbnails/50/${avatarId}.png`;
             document.getElementById('avatar').src = avatarUrl;
-            document.getElementById('avatar').style.display = 'block';  // Tornar a imagem visível
+            document.getElementById('avatar').style.display = 'block';
             document.getElementById('welcomeMessage').innerText = `Olá, ${userName}!`;
 
-            // Buscar dados de user-power-data usando avatarId
             const powerDataResponse = await fetch(`https://summer-night-03c0.rk-foxx-159.workers.dev/?https://rollercoin.com/api/profile/user-power-data/${avatarId}`);
             const powerData = await powerDataResponse.json();
-            const miners = powerData.data.miners; // Acessando diretamente powerData.data
+            const miners = powerData.data.miners;
             let bonusPercent = powerData.data.bonus_percent;
 
-            // Processar bonus_percent
-            bonusPercent = parseFloat((bonusPercent / 100).toFixed(2));  // Dividir por 100 e garantir que é um número
+            bonusPercent = parseFloat((bonusPercent / 100).toFixed(2));
 
-            // Calcular Poder Total
             const total_orig = miners * (1 + (bonusPercent / 100));
 
-            // Buscar dados de room-config usando avatarId
             const roomConfigResponse = await fetch(`https://summer-night-03c0.rk-foxx-159.workers.dev/?https://rollercoin.com/api/game/room-config/${avatarId}`);
             const roomConfigData = await roomConfigResponse.json();
-            const minerData = roomConfigData.data.miners; // Acessando diretamente roomConfigData.data
-            const racks = roomConfigData.data.racks; // Supondo que racks seja parte do roomConfigData.data
+            const minerData = roomConfigData.data.miners;
+            const racks = roomConfigData.data.racks;
 
-            // Associar cada minerador à sua rack
             minerData.forEach(miner => {
-                const rack = racks.find(r => r.rack_id === miner.rack_id); // Encontrar a rack correspondente
+                const rack = racks.find(r => r._id === miner.placement.user_rack_id);
                 if (rack) {
-                    miner.rack_info = rack; // Adiciona as informações da rack ao miner
+                    miner.placement.rack_info = rack;
                 }
             });
 
-            // Filtrar minerData com base na opção selecionada
             const selectedOption = document.querySelector('input[name="option"]:checked').value;
             let filteredMiners = minerData;
             if (selectedOption === 'op1') {
@@ -86,7 +77,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 filteredMiners = minerData.filter(miner => miner.width === 2);
             }
 
-            // Contar as repetições de miner_id
             function countRepetitions(minerIds) {
                 const counts = minerIds.reduce((acc, id) => {
                     acc[id] = (acc[id] || 0) + 1;
@@ -95,15 +85,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 return counts;
             }
 
-            // Array com os miner_id dos mineradores filtrados
             const minerIds = filteredMiners.map(miner => miner.miner_id);
-
-            // Contar as repetições
             const counts = countRepetitions(minerIds);
 
-            // Calcular newpower para cada miner e armazenar todos os mineradores negativos
             const results = filteredMiners.map(miner => {
-                // Ajustar o cálculo do newBonusPercent com base nas repetições
                 const newBonusPercent = counts[miner.miner_id] > 1 ? bonusPercent : (bonusPercent - (miner.bonus_percent / 100));
                 const newpower = (((miners - miner.power) * (1 + (newBonusPercent / 100))) - total_orig);
                 
@@ -126,12 +111,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 newpower: convertPower(miner.newpower)                
             })));
 
-            // Atualizar os elementos da página com informações dos mineradores negativos
             const updateElement = (index, miner) => {
                 if (miner) {
                     const levelInfo = getLevelDescription(miner.level);
                     const levelSpan = `<span style="color: ${levelInfo.color}; font-weight: bold;">${levelInfo.text}</span> ${miner.name}`;
-                    document.getElementById(`nome${index}`).innerHTML = levelSpan; // Usar innerHTML para aplicar o estilo
+                    document.getElementById(`nome${index}`).innerHTML = levelSpan;
                     document.getElementById(`img${index}`).src = `https://static.rollercoin.com/static/img/market/miners/${miner.filename}.gif?v=1`;
                     document.getElementById(`img${index}`).style.display = 'block';
                     document.getElementById(`poder${index}`).innerText = convertPower2(miner.power);
@@ -140,9 +124,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     document.getElementById(`set${index}`).innerText = miner.is_in_set ? 'Sim' : 'Não';
                     document.getElementById(`merge${index}`).innerText = counts[miner.miner_id] > 1 ? 'Sim' : 'Não';
 
-                    // Exibir informações da rack
-                    if (miner.rack_info) {
-                        const rack = miner.rack_info;
+                    if (miner.placement.rack_info) {
+                        const rack = miner.placement.rack_info;
                         document.getElementById(`rack${index}`).innerText = `Sala: ${rack.room_level + 1}, Linha: ${rack.placement.x + 1}, Rack: ${rack.placement.y + 1}`;
                     }
                 } else {
@@ -150,16 +133,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             };
 
-            updateElement(1, top10NegativeResults[0]);
-            updateElement(2, top10NegativeResults[1]);
-            updateElement(3, top10NegativeResults[2]);
-            updateElement(4, top10NegativeResults[3]);
-            updateElement(5, top10NegativeResults[4]);
-            updateElement(6, top10NegativeResults[5]);
-            updateElement(7, top10NegativeResults[6]);
-            updateElement(8, top10NegativeResults[7]);
-            updateElement(9, top10NegativeResults[8]);
-            updateElement(10, top10NegativeResults[9]);
+            top10NegativeResults.forEach((miner, i) => updateElement(i + 1, miner));
 
         } catch (error) {
             console.error('Erro ao buscar dados:', error);
