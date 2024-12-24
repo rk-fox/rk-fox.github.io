14679e41458780cf0858debdc276fa648c86b3e5
rk-fox <161100607+rk-fox@users.noreply.github.com>
2024-12-23 22:02:21 -0300

    Update impact.js

--- impacto2/impact.js
+++ impacto2/impact.js
@@ -1,37 +1,39 @@
 document.getElementById('searchButton').addEventListener('click', async () => {
-        const userLink = document.getElementById('linkInput').value;
+    const userLink = document.getElementById('linkInput').value;
 
-        if (!userLink) {
-            alert('Por favor, digite o link da sala.');
+    if (!userLink) {
+        alert('Por favor, digite o link da sala.');
+        return;
+    }
+
+    try {
+        const profileResponse = await fetch(`https://summer-night-03c0.rk-foxx-159.workers.dev/?https://rollercoin.com/api/profile/public-user-profile-data/${userLink}`); 
+        const profileData = await profileResponse.json();
+        const userName = profileData.data.name; 
+        const avatarId = profileData.data.avatar_id;
+
+        if (!avatarId || !userName) {
+            alert('Erro ao obter o avatar_id ou nome.');
             return;
         }
 
-        try {
-            const profileResponse = await fetch(`https://summer-night-03c0.rk-foxx-159.workers.dev/?https://rollercoin.com/api/profile/public-user-profile-data/${userLink}`); 
-            const profileData = await profileResponse.json();
-            const userName = profileData.data.name; 
-            const avatarId = profileData.data.avatar_id;
+        const avatarUrl = `https://avatars.rollercoin.com/static/avatars/thumbnails/50/${avatarId}.png`;
+        document.getElementById('avatar').src = avatarUrl;
+        document.getElementById('avatar').style.display = 'block';
+        document.getElementById('welcomeMessage').innerText = `Olá, ${userName}!`;
 
-            if (!avatarId || !userName) {
-                alert('Erro ao obter o avatar_id ou nome.');
-                return;
-            }
+        const powerDataResponse = await fetch(`https://summer-night-03c0.rk-foxx-159.workers.dev/?https://rollercoin.com/api/profile/user-power-data/${avatarId}`);
+        const powerData = await powerDataResponse.json();
+        const miners = powerData.data.miners;
+        let bonusPercent = powerData.data.bonus_percent;
 
-            const avatarUrl = `https://avatars.rollercoin.com/static/avatars/thumbnails/50/${avatarId}.png`;
-            document.getElementById('avatar').src = avatarUrl;
-            document.getElementById('avatar').style.display = 'block';
-            document.getElementById('welcomeMessage').innerText = `Olá, ${userName}!`;
+        bonusPercent = parseFloat((bonusPercent / 100).toFixed(2));
 
-            const powerDataResponse = await fetch(`https://summer-night-03c0.rk-foxx-159.workers.dev/?https://rollercoin.com/api/profile/user-power-data/${avatarId}`);
-            const powerData = await powerDataResponse.json();
-            const miners = powerData.data.miners;
-            let bonusPercent = powerData.data.bonus_percent;
-
-            bonusPercent = parseFloat((bonusPercent / 100).toFixed(2));
-
-            const total_orig = miners * (1 + (bonusPercent / 100));
+        const total_orig = miners * (1 + (bonusPercent / 100));
 
         console.log("Miners Data:", miners);
         console.log("Bonus Data:", bonusPercent);
-                
-    });
+    } catch (error) {
+        console.error("Erro ao obter dados da API:", error);
+    }
+});
