d1c9e00ee90780df5e7146e9e47ce88f815fae24
rk-fox <161100607+rk-fox@users.noreply.github.com>
2025-01-04 12:14:47 -0300

    Update salaperfeita.js

--- salaperfeita/salaperfeita.js
+++ salaperfeita/salaperfeita.js
@@ -155,7 +155,7 @@ while ((match = minerRegex.exec(cleanedField2)) !== null) {
         Set: set,                // Set (entre "Set" e "Size")
         Size: match[4],          // Size (já capturado em match[4])
         Power: power,            // Power (em Gh/s) com 3 casas decimais
-        Bonus: match[7], // Bonus
+        Bonus: match[7].replace('.', ','), // Bonus
         Quantity: match[8]       // Quantity
     };
 
