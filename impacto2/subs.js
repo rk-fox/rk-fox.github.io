15cbd7d84a9501c4fa56d29e88e2b9ba69570271
rk-fox <161100607+rk-fox@users.noreply.github.com>
2025-01-11 21:08:36 -0300

    Update subs.js

--- impacto2/subs.js
+++ impacto2/subs.js
@@ -94,24 +94,26 @@ async function loadGoogleSheetData() {
             return [];
         }
 
-        // Extraímos apenas as colunas desejadas e exibimos o resultado
+
+// Extraímos apenas as colunas desejadas e exibimos o resultado
 result = data.values.map(row => [
     row[0], 
-    row[1] !== "-" && row[1] !== "#N/A" ? row[1].replace('.', '') : row[1],
-    row[2].replace('.', ','), 
-    row[23] !== "-" && row[23] !== "#N/A" ? row[23].replace('.', '') : row[23],
-    row[28].replace('.', ','), 
-    row[24] !== "-" && row[24] !== "#N/A" ? row[24].replace('.', '') : row[24],
-    row[29].replace('.', ','), 
-    row[25] !== "-" && row[25] !== "#N/A" ? row[25].replace('.', '') : row[25],
-    row[30].replace('.', ','), 
-    row[26] !== "-" && row[26] !== "#N/A" ? row[26].replace('.', '') : row[26],
-    row[31].replace('.', ','), 
-    row[27] !== "-" && row[27] !== "#N/A" ? row[27].replace('.', '') : row[27],
-    row[32].replace('.', ','), 
-    row[38] !== "-" && row[38] !== "#N/A" ? row[38].replace('.', '') : row[38],
-    row[39].replace('.', ','),
+    row[1] && row[1] !== "-" && row[1] !== "#N/A" ? row[1].replace('.', '') : row[1] || '',
+    row[2] ? row[2].replace('.', ',') : '',
+    row[23] && row[23] !== "-" && row[23] !== "#N/A" ? row[23].replace('.', '') : row[23] || '',
+    row[28] ? row[28].replace('.', ',') : '',
+    row[24] && row[24] !== "-" && row[24] !== "#N/A" ? row[24].replace('.', '') : row[24] || '',
+    row[29] ? row[29].replace('.', ',') : '',
+    row[25] && row[25] !== "-" && row[25] !== "#N/A" ? row[25].replace('.', '') : row[25] || '',
+    row[30] ? row[30].replace('.', ',') : '',
+    row[26] && row[26] !== "-" && row[26] !== "#N/A" ? row[26].replace('.', '') : row[26] || '',
+    row[31] ? row[31].replace('.', ',') : '',
+    row[27] && row[27] !== "-" && row[27] !== "#N/A" ? row[27].replace('.', '') : row[27] || '',
+    row[32] ? row[32].replace('.', ',') : '',
+    row[38] && row[38] !== "-" && row[38] !== "#N/A" ? row[38].replace('.', '') : row[38] || '',
+    row[39] ? row[39].replace('.', ',') : '',
 ]);
+
 
 
         // Exibindo o resultado no console
