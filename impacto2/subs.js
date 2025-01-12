3758b781136928cc381978301b1a37dcbf8fe308
rk-fox <161100607+rk-fox@users.noreply.github.com>
2025-01-11 22:27:05 -0300

    Update subs.js

--- impacto2/subs.js
+++ impacto2/subs.js
@@ -1,3 +1,14 @@
+// Recuperar os valores do localStorage
+const minersPower2 = localStorage.getItem('minersPower2');
+const totalbonusPercent2 = localStorage.getItem('totalbonusPercent2');
+const total_orig2 = localStorage.getItem('total_orig2');
+const subimpactArray = JSON.parse(localStorage.getItem('subimpactArray'));
+
+console.log(minersPower2);  // Vai exibir o valor de minersPower2
+console.log(totalbonusPercent2);  // Vai exibir o valor de totalbonusPercent2
+console.log(total_orig2);  // Vai exibir o valor de total_orig2
+console.log(subimpactArray);  // Vai exibir o valor de subimpactArray
+
 let result = [];
 
 // Função para converter valores de poder (GH, TH, PH)
