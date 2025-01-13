56f9e6a27bf1d3f894f63527bfca6bc046231578
rk-fox <161100607+rk-fox@users.noreply.github.com>
2025-01-13 13:52:09 -0300

    Update contador.js

--- impacto/contador.js
+++ impacto/contador.js
@@ -1,13 +1,13 @@
 const script_google = 'https://script.google.com/macros/s/AKfycbyiwGM4G3QVt0DQTPCKvpR0QglskRcnxCDmS_CfC4Ho-A5pUAvP5eAa0ehw27B-BMDM/exec';
 
-// Atualizar a célula A2
-function updateCounterA2() {
-    fetch(`${script_google}?action=updateA2`, {
+// Atualizar a célula B2
+function updateCounterB2() {
+    fetch(`${script_google}?action=updateB2`, {
         method: 'POST',
         mode: 'no-cors'
     })
-    .then(() => console.log('A2 atualizado com sucesso'))
-    .catch(error => console.error('Erro:', error));
+    //.then(() => console.log('B2 atualizado com sucesso'))
+    //.catch(error => console.error('Erro:', error));
 }
 
 // Atualiza A2 quando o site for carregado
