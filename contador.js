555f34a4fbf527e3f6840cdc06cba6c4b2ebf5ec
rk-fox <161100607+rk-fox@users.noreply.github.com>
2025-01-13 11:15:15 -0300

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,14 +1,11 @@
-const script_google = 'https://script.google.com/macros/s/AKfycbzQHIXDtfr5mNxm0EvtyUPvEBKl4c6kCa8-N-lf_VjoOQTkIG1RFNKsl0rdQrYN1DJm/exec' ;
-const dados_cont = 1;
+const script_google = 'https://script.google.com/macros/s/AKfycbzQHIXDtfr5mNxm0EvtyUPvEBKl4c6kCa8-N-lf_VjoOQTkIG1RFNKsl0rdQrYN1DJm/exec';
 
-dados_cont.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    fetch(script_google, { method: 'POST', body: new FormData(dados_cont) })
+document.addEventListener('DOMContentLoaded', function () {
+    fetch(script_google, { method: 'POST' })
     .then(response => {
-        alert('Contador atualizado', response);
+        alert('Contador atualizado');
     })
     .catch(error => {
-        console.error('Erro no contador', error);
-});
+        console.error('Erro ao atualizar contador', error);
+    });
 });
