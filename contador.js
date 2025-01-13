9c7e45f52c782e8c1e33a8cc1ba8b720acc64624
rk-fox <161100607+rk-fox@users.noreply.github.com>
2025-01-13 11:56:54 -0300

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,8 +1,8 @@
 const script_google = 'https://script.google.com/macros/s/AKfycbzQHIXDtfr5mNxm0EvtyUPvEBKl4c6kCa8-N-lf_VjoOQTkIG1RFNKsl0rdQrYN1DJm/exec';
 
 document.addEventListener('DOMContentLoaded', function () {
-    fetch(script_google, { method: 'POST' })
-    .then(response => {
+    fetch(script_google, { method: 'POST', mode: 'no-cors' })
+    .then(() => {
         alert('Contador atualizado');
     })
     .catch(error => {
