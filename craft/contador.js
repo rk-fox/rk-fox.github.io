238166b51cd0f07405a308132ae803e624c32be5
rk-fox <161100607+rk-fox@users.noreply.github.com>
2025-01-13 15:47:14 -0300

    Update contador.js

--- craft/contador.js
+++ craft/contador.js
@@ -1,21 +1,14 @@
 const script_google = 'https://script.google.com/macros/s/AKfycbwcaI45b-FgWqcCjgyqkKQnxVhlTw8z2NDL0lggfTwOH0gVIJoThIIatDHV912fsVPu/exec';
 
-// Função para fazer a requisição e chamar a função do Google Apps
-function updateCounter() {
+// Atualizar a célula A2
+function updateCounterA2() {
     fetch(`${script_google}`, {
         method: 'POST',
-        mode: 'no-cors'  // Usado para fazer a requisição sem precisar de resposta
-    })
-    .then(response => {
-        // Se a requisição for bem-sucedida, você pode fazer o que quiser com a resposta
-        console.log('Contador atualizado com sucesso.');
-    })
-    .catch(error => {
-        console.error('Erro ao fazer a requisição:', error);
+        mode: 'no-cors'
     });
 }
 
-// Chama a função quando o site é carregado
+// Atualiza A2 quando o site for carregado
 document.addEventListener('DOMContentLoaded', function () {
     updateCounter();
 });
