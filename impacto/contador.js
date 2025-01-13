a3e037953badfa40eb593ad2b8f064832295efba
rk-fox <161100607+rk-fox@users.noreply.github.com>
2025-01-13 14:34:54 -0300

    Update contador.js

--- impacto/contador.js
+++ impacto/contador.js
@@ -1,8 +1,8 @@
-const script_google = 'https://script.google.com/macros/s/AKfycbyiwGM4G3QVt0DQTPCKvpR0QglskRcnxCDmS_CfC4Ho-A5pUAvP5eAa0ehw27B-BMDM/exec';
+const script_google = 'https://script.google.com/macros/s/AKfycbw9TfgggqeY_ByvmDb15Vgi6DfOaPjc5FyIb_yCjkMBIXE7toViYYj1UerBJw6KUcWP/exec';
 
 // Atualizar a célula B2
 function updateCounterB2() {
-    fetch(`${script_google}?path=updateB2`, {
+    fetch(`${script_google}`, {
         method: 'POST',
         mode: 'no-cors'
     });
