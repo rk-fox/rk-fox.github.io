1c42cae26e25f61ef923520eaff650135654dc11
rk-fox <161100607+rk-fox@users.noreply.github.com>
2026-09-22 16:08:57 -0300

    Update RoomOrganizer.tsx

--- pages/RoomOrganizer.tsx
+++ pages/RoomOrganizer.tsx
@@ -1103,16 +1103,17 @@ export const RoomOrganizer: React.FC = () => {
                     return (b.marginalImpact ?? b.realPower ?? 0) - (a.marginalImpact ?? a.realPower ?? 0);
                 case 'real_power_asc':
                     return (a.marginalImpact ?? a.realPower ?? 0) - (b.marginalImpact ?? b.realPower ?? 0);
-                case 'mais_poder_menos_bonus': {
-                    const ratioA = a.power / (a.bonus_percent + 0.001);
-                    const ratioB = b.power / (b.bonus_percent + 0.001);
-                    return ratioB - ratioA;
+                                case 'mais_poder_menos_bonus': {
+                    const diffPower = b.power - a.power;
+                    if (diffPower !== 0) return diffPower;
+                    return a.bonus_percent - b.bonus_percent;
                 }
                 case 'mais_bonus_menos_poder': {
-                    const ratioA = a.bonus_percent / (a.power + 1);
-                    const ratioB = b.bonus_percent / (b.power + 1);
-                    return ratioB - ratioA;
+                    const diffBonus = b.bonus_percent - a.bonus_percent;
+                    if (diffBonus !== 0) return diffBonus;
+                    return a.power - b.power;
                 }
+
                 case 'power_desc':
                     return b.power - a.power;
                 case 'power_asc':
